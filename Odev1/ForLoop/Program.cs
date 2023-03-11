676a7c7df67e66cab92c1d053fe2c673c8a05580
Fatma <fmutlu3002@gmail.com>
2023-03-11 20:12:07 +0300

    Add project files.

---
+++ Odev1/ForLoop/Program.cs
@@ -0,0 +1,59 @@
+﻿//dizideki en büyük ve en küçük elemanı bulup iki sayının ortalamasını diziye yeni eleman olarak ekleyen ve diziyi kabarcık sıralamaya göre sıralayan program
+
+int[] array = { 24, 6, 52, 38, 76, 94 };
+
+Console.Write("Dizi:");
+foreach (var item in array)
+{
+    Console.Write($"{item} ");
+}
+
+List<int> arrayList = new List<int>();
+
+int smallestNumber = array[0];
+int greaterNumber = array[0];
+
+// find smallest and greater number with for loop
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] < smallestNumber)
+    {
+        smallestNumber = array[i];
+    }
+
+    if (array[i] > greaterNumber)
+    {
+        greaterNumber = array[i];
+    }
+    //for our new array which has average element
+    arrayList.Add(array[i]);
+}
+
+Console.WriteLine($"\n Dizideki en küçük sayı:{smallestNumber}, dizideki en büyük sayı:{greaterNumber}");
+//find average
+int average = (smallestNumber + greaterNumber) / 2;
+
+//our new array includes average element.
+arrayList.Add(average);
+int[] newArray = arrayList.ToArray();
+
+//sorting array with bubble sort
+int temp;
+for (int i = 0; i < newArray.Length - 1; i++)
+{
+    for (int j = 0; j < newArray.Length - 1 - i; j++)
+    {
+        if (newArray[j] > newArray[j + 1])
+        {
+            temp = newArray[j];
+            newArray[j] = newArray[j + 1];
+            newArray[j + 1] = temp;
+        }
+    }
+}
+
+Console.WriteLine("\n Kabarcık sıralama ile küçükten büyüğe sıralanmış dizi:");
+foreach (var item in newArray)
+{
+    Console.WriteLine(item);
+}
---
+++ Odev1/WhileLoop/Program.cs
@@ -0,0 +1,78 @@
+﻿//Verilen dizideki en büyük ve en küçük elemanın ortalamasını bularak ortalamayı dizideki herbir elemandan çıkartıp elde edilen diziyi tersine çeviren program.
+
+int[] array = { 25, 6, 52, 33, 79, 94 };
+Console.WriteLine("Dizi: 25, 6, 52, 33, 79, 94");
+
+int count = 0;
+int smallestNumber = array[0];
+int greatestNumber = array[0];
+int index = 0;
+
+//find smallest and greatest element in array
+while (count < array.Length)
+{
+    if (array[index] < smallestNumber)
+    {
+        smallestNumber = array[index];
+    }
+
+    if (array[index] > greatestNumber)
+    {
+        greatestNumber = array[index];
+    }
+    index++;
+    count++;
+}
+
+Console.WriteLine($"\n Dizideki en küçük sayı:{smallestNumber}, dizideki en büyük sayı:{greatestNumber}");
+
+int average = (smallestNumber + greatestNumber) / 2;
+Console.WriteLine($"\n Ortalama:{average}");
+int i = 0;
+
+//subtract the average from each element in array
+foreach (var item in array)
+{
+    array[i++] -= average;
+
+}
+
+//sort array element ascending
+int temp;
+for (int j = 0; j < array.Length - 1; j++)
+{
+    for (int k = 0; k < array.Length - 1 - j; k++)
+    {
+        if (array[k] > array[k + 1])
+
+        {
+            temp = array[k];
+            array[k] = array[k + 1];
+            array[k + 1] = temp;
+        }
+    }
+}
+
+Console.WriteLine("\nEn büyük sayı ile en küçük sayının aritmetik ortalaması her bir elemenden çıkartılarak oluşturulmuş yeni dizi:");
+foreach (var item in array)
+{
+    Console.WriteLine(item);
+}
+
+
+// function for reversing the array(descending)
+int temp2;
+for (int l = 0; l < array.Length / 2; l++)
+{
+    temp2 = array[array.Length - l - 1];
+    array[array.Length - l - 1] = array[l];
+    array[l] = temp2;
+}
+
+
+Console.WriteLine("\nTersine çevrilmiş yeni dizi:");
+//show array element on console
+foreach (var item in array)
+{
+    Console.WriteLine(item);
+}
