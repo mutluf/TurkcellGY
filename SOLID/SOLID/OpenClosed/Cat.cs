ffce39e17019658992e00c5b9747ca8e6dae2301
Fatma <fmutlu3002@gmail.com>
2023-03-28 22:06:49 +0300

    Open Closed

---
+++ SOLID/SOLID/OpenClosed/Cat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenClosed
+{
+    public class Cat : IAnimal
+    {
+        public void React()
+        {
+            Console.WriteLine("Miyav der gelir.");
+        }
+    }
+}
---
+++ SOLID/SOLID/OpenClosed/Program.cs
@@ -0,0 +1,14 @@
+﻿using OpenClosed;
+
+IAnimal animal = new Bear();
+
+if (animal.GetType().Name == "Cat")
+{
+    animal.React();
+    Console.WriteLine("Kaçmana gerek yok.");
+}
+else if (animal.GetType().Name == "Bear")
+{
+    animal.React();
+    Console.WriteLine("Kaç!");
+}
