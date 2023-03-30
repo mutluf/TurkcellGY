c7e408c622b0dd9872dbd14b5841dc41fcfb3153
Fatma <fmutlu3002@gmail.com>
2023-03-30 09:58:12 +0300

    Liskov Substitution

---
+++ SOLID/SOLID/LiskovSubstitution/RentABuilding.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liskov
+{
+    public class RentABuilding : IRentABuilding
+    {
+        public void BuildingAge()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DatePick()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SOLID/SOLID/LiskovSubstitution/RentACar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liskov
+{
+    public class RentACar : IRentACar
+    {
+        public void CarKilometer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DatePick()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
